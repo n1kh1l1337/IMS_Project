4dd6475ecd1700a058da6fdaf7bf0f3c01383b4e
Nikhil Sharma <jangidnik@gmail.com>
2021-10-11 18:01:56 +0530

    Grammatical Mistake

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -2,5 +2,5 @@
 @section('page_title', 'My Dashboard')
 
 @section('content')
-    <h2>WELCOME {{ Auth::user()->name }}. This is your DASHBOARD</h2>
+    <h2>Welcome {{ Auth::user()->name }}. That's your Dashboard. </h2>
     @endsection
